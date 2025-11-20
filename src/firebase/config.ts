f30a394a3cebf85a2eb55b04e16f4133509f12a6
DHANYAA M R <dhanyaamr.it25@bitsathy.ac.in>
2025-11-20 03:38:48 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // This is a public config, so it's safe to expose.
 // Your rules in `firestore.rules` will protect your data.
 export const firebaseConfig = {
-  apiKey: "TODO_FROM_FIREBASE_CONSOLE",
-  authDomain: "TODO_FROM_FIREBASE_CONSOLE",
-  projectId: "TODO_FROM_FIREBASE_CONSOLE",
-  storageBucket: "TODO_FROM_FIREBASE_CONSOLE",
-  messagingSenderId: "TODO_FROM_FIREBASE_CONSOLE",
-  appId: "TODO_FROM_FIREBASE_CONSOLE"
+  apiKey: "AIzaSyA3evAH1ACKzovbPCv61N8HTPtZnPFs9v8",
+  authDomain: "studio-6608661919-200ba.firebaseapp.com",
+  projectId: "studio-6608661919-200ba",
+  storageBucket: "studio-6608661919-200ba.appspot.com",
+  messagingSenderId: "468034656632",
+  appId: "1:468034656632:web:3257bbd6ed37b02871508d"
 };
