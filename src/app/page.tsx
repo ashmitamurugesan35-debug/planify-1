a9d5fb8f1c2f4fef5abd5327931207ad59fe636c
DHANYAA M R <dhanyaamr.it25@bitsathy.ac.in>
2025-11-21 05:43:49 +0000

    UNDO THE CHANGES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     }
   }, [status, router]);
 
-  const backgroundImage = PlaceHolderImages.find(img => img.id === 'welcome-page-background');
+  const backgroundImage = PlaceHolderImages.find(img => img.id === 'blurred-desk-background');
 
   if (status === 'loading' || status === 'authenticated') {
     return (
